3a67fb9d1014e15cd9dc88e163cae042ee3156bd
olegnizh <olegnizh@mail.ru>
2023-12-14 21:40:30 +0300

    4.1.15

---
+++ ConsoleApp4115/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp4115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 6;
+            var b = 7;
+
+            var c = a != b ? a + b : b;
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
