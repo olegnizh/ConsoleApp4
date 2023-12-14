6e83955505d7748973a80212f6612035a4bb3893
Нижегородов Олег Эдуардович <olegnizh@mail.ru>
2023-12-14 18:01:00 +0300

    4.1.5

---
+++ ConsoleApp415/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp415
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 2;
+            double X = 1.3;
+            double Y = 1.5;
+
+            bool C = (A < B) | (X > Y);
+            Console.WriteLine(C);
+
+
+        }
+    }
+}
