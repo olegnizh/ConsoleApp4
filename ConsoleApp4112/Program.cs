854cff64e7922f1345e14d0733aa95cb31efb483
Нижегородов Олег Эдуардович <olegnizh@mail.ru>
2023-12-14 18:08:36 +0300

    4.1.12

---
+++ ConsoleApp4112/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4112
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 8;
+            var b = 8;
+
+            if (a == b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+
+
+
+        }
+    }
+}
