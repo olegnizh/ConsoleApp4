8ccb68f4420f0cf63c5aed8e418c2b83f665004f
olegnizh <olegnizh@mail.ru>
2023-12-14 21:43:09 +0300

    4.1.17

---
+++ ConsoleApp4117/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp4117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color1 = Console.ReadLine();
+            if (color1 == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+            }
+            else if (color1 == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is cyan!");
+            }
+
+
+        }
+    }
+}
