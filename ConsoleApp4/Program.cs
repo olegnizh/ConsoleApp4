484dcc597da7987959b30a389b7b9dfffd998869
Нижегородов Олег Эдуардович <olegnizh@mail.ru>
2023-12-14 17:55:15 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "a";
+            string B = "v";
+            bool C = A != B;
+            Console.WriteLine(C);
+
+        }
+    }
+}
