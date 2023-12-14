1fda72813349f701489c0b4fdb15b59071a2cc7c
olegnizh <olegnizh@mail.ru>
2023-12-14 21:26:41 +0300

    4.1.11

---
+++ ConsoleApp4111/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp4111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var a = 6;
+			var b = 6;
+
+			if (a == b)
+			{
+				Console.WriteLine("Условие истинно");
+			}
+			else
+			{
+				Console.WriteLine("Условие ложно");
+			}
+
+
+		}
+    }
+}
