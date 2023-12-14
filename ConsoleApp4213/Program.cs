6b1c1262679b7b8d7381fccd599957d6bd7dcfa0
olegnizh <olegnizh@mail.ru>
2023-12-15 00:12:07 +0300

    4.2.13

---
+++ ConsoleApp4213/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp4213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Цикл while");
+            int k = 0;
+            while (k < 0)
+            {
+                Console.WriteLine(k);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+                }
+                k++;
+            }
+
+            Console.WriteLine("Цикл do");
+            int t = 0;
+            do
+            {
+                Console.WriteLine(t);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+                }
+                t++;
+            } while (t < -1);
+
+
+        }
+    }
+}
