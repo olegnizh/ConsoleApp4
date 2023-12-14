7ac70d4b7a4350d5b2c3f8130c34d2da508d57cd
olegnizh <olegnizh@mail.ru>
2023-12-14 23:26:55 +0300

    4.2.9

---
+++ ConsoleApp429/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace ConsoleApp429
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // 1
+            Console.WriteLine("Цикл с предусловием");
+            int k = 0;
+            while (k < 3)
+            {
+                Console.WriteLine(k);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        k++;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        k++;
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        k++;
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            // 2
+            Console.WriteLine("Цикл с постусловием");
+            int x = 0;
+            do
+            {
+                if (x == 3)
+                    break;
+
+                Console.WriteLine(x);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        x++;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        x++;
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        x++;
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        break;
+                }
+            } while (true);
+            
+            // 3
+            Console.WriteLine("Цикл for");
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
