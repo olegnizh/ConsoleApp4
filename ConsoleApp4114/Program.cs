b5c54f20dba488a0bd5935ac14196166813bf2e0
olegnizh <olegnizh@mail.ru>
2023-12-14 21:35:51 +0300

    4.1.14

---
+++ ConsoleApp4114/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var a = 6;
+			var b = 7;
+
+			if (a == b && b > 1)
+			{
+				Console.WriteLine("Условие истинно");
+			}
+
+			else if (b > 10 || b == 7)
+			{
+				Console.WriteLine("Значение b = {0} больше 10 или равно 7", b);
+			}
+			else
+			{
+				Console.WriteLine("Значение b = {0}", b);
+			}
+
+
+		}
+    }
+}
