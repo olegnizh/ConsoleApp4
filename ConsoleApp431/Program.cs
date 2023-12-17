bc6e34cb632e56ed0637fd1feedf468d5ed3abad
olegnizh <olegnizh@mail.ru>
2023-12-18 01:12:33 +0300

    4.3.1

---
+++ ConsoleApp431/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp431
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favcolors = new string[3];
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                Console.WriteLine("Введите любимый цвет номер {0}", i + 1);
+                favcolors[i] = Console.ReadLine();
+            }
+
+            foreach (var color in favcolors)
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+ 
+                }
+
+                    
+            
+            }
+                   
+            
+
+
+            }
+    }
+}
