b49307dc48344eb1848eb7274c3680665f58a047
olegnizh <olegnizh@mail.ru>
2023-12-18 00:40:07 +0300

    4.2.15

---
+++ ConsoleApp4215/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp4215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (true)
+            {
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                Console.WriteLine(n);
+                var text = Console.ReadLine();
+                if (text == "stop")
+                {
+                    Console.WriteLine("Цикл остановлен");
+                    break;
+                }
+
+                switch (text)
+                {
+                    case "red":
+                        n++;
+                        Console.WriteLine("Вы иыбрали номером {0} цвет {1}", n, text);
+                        break;
+                    case "green":
+                        n++;
+                        Console.WriteLine("Вы иыбрали номером {0} цвет {1}", n, text);
+                        break;
+                    case "cyan":
+                        n++;
+                        Console.WriteLine("Вы иыбрали номером {0} цвет {1}", n, text);
+                        break;
+                    default:
+                        //Console.WriteLine("Вы не выбрали ничего");
+                        continue;
+                       
+                }
+            }
+
+
+        }
+    }
+}
