cd43ccee85ee1d30faff62f5810bdd1499bed215
olegnizh <olegnizh@mail.ru>
2023-12-18 01:19:50 +0300

    4.3.5

---
+++ ConsoleApp435/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp435
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("Ваше имя по буквам");            
+            foreach (var ch in name)
+            {
+                Console.Write(ch + " ");
+            }
+            Console.Write("Последняя буква {0}", name[name.Length - 1]);
+        }
+
+
+    }
+}
