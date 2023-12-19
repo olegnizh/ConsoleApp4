3a4df0f71cacfcec14131f83c1249961dd131c39
olegnizh <olegnizh@mail.ru>
2023-12-19 21:29:34 +0300

    4.3.10

---
+++ ConsoleApp4310/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };            
+
+            Console.WriteLine(" ");
+
+            Console.Write("Количество строк: ");
+            Console.WriteLine(array.GetUpperBound(0) + 1 + " ");
+
+            Console.Write("Количество колонок: ");
+            Console.WriteLine(array.GetUpperBound(1) + 1 + " ");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
