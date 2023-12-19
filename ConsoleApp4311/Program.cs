17c9f02522a138fd8440c7ed4bca74d607449220
olegnizh <olegnizh@mail.ru>
2023-12-19 21:35:23 +0300

    4.3.11

---
+++ ConsoleApp4311/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp4311
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+            Console.WriteLine();
+            for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+            {
+                for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+                     Console.Write(array[i, k] + " ");
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
