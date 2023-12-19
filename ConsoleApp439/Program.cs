6925ce6e9354c96d86d83a3a1824149b3fc6529a
olegnizh <olegnizh@mail.ru>
2023-12-19 21:23:18 +0300

    4.3.9

---
+++ ConsoleApp439/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp439
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+
+        }
+    }
+}
